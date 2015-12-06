b5d468baa3d6766c54edd127df71bfccdd3996a1
Adam Draper <adamwdraper@gmail.com>
2015-12-06 13:58:49 -0800

    space with 2

--- public/javascripts/utilities/storage/utility.js
+++ public/javascripts/utilities/storage/utility.js
@@ -1,26 +1,26 @@
 define([
-    'underscore',
-    'backbone'
+  'underscore',
+  'backbone'
 ], function(_, Backbone) {
-    var View = Backbone.View.extend({
-            events: {},
-            initialize: function() {},
-            render: function() {
-                return this;
-            },
-            set: function(name, value) {
-                window.localStorage.setItem(name, JSON.stringify(value));
-            },
-            get: function(name) {
-                return JSON.parse(window.localStorage.getItem(name));
-            },
-            unset: function(name) {
-                window.localStorage.removeItem(name);
-            },
-            clear: function() {
-                localStorage.clear();
-            }
-        });
+  var View = Backbone.View.extend({
+    events: {},
+    initialize: function() {},
+    render: function() {
+      return this;
+    },
+    set: function(name, value) {
+      window.localStorage.setItem(name, JSON.stringify(value));
+    },
+    get: function(name) {
+      return JSON.parse(window.localStorage.getItem(name));
+    },
+    unset: function(name) {
+      window.localStorage.removeItem(name);
+    },
+    clear: function() {
+      localStorage.clear();
+    }
+  });
 
-    return new View();
+  return new View();
 });
