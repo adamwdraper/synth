08c3ea129180805b728d3d37ab297704bf76835a
Adam Draper <adamwdraper@gmail.com>
2016-01-01 21:25:15 -0800

    add listing

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
+var packageJSON  = require('./package');
 var sass = require('gulp-sass');
+var jshint = require('gulp-jshint');
+var jshintConfig = packageJSON.jshintConfig;
+
+jshintConfig.lookup = false;
+
+gulp.src('yo').pipe(jshint(jshintConfig));
 
 gulp.task('default', function() {
   // place code for your default task here
@@ -11,6 +18,17 @@ gulp.task('sass', function () {
       outputStyle: 'compressed'
     }).on('error', sass.logError))
     .pipe(gulp.dest('./public/stylesheets'));
+});
+
+gulp.task('lint', function() {
+  return gulp.src([
+      './public/javascripts/routers/*.js',
+      './public/javascripts/components/*.js',
+      './public/javascripts/plugins/*.js',
+      './public/javascripts/utilities/*.js'
+    ])
+    .pipe(jshint(jshintConfig))
+    .pipe(jshint.reporter('default'));
 });
 
 gulp.task('sass:watch', function () {
