6e649fc48de6718eb7d2105ccd8ad9d1226c62a5
Adam Draper <adamwdraper@gmail.com>
2016-01-04 14:58:59 -0800

    update indentation

--- public/javascripts/libraries/require/configs/dev.js
+++ public/javascripts/libraries/require/configs/dev.js
@@ -2,38 +2,38 @@
  * Dev Config Settings
  */
 requirejs.config({
-    waitSeconds: 0,
-    baseUrl: 'javascripts',
-    config: {
-        'appular': {
-            'env': 'dev'
-        }
-    },
-    paths: {
-        'appular': 'libraries/appular/appular',
-        'backbone': 'libraries/backbone/backbone',
-        'backboneStickit': 'libraries/backbone/extensions/stickit',
-        'domReady': 'libraries/require/plugins/domReady',
-        'jquery': 'libraries/jquery/jquery',
-        'jqueryFunctions': 'libraries/jquery/extensions/functions',
-        'log': 'libraries/log/log',
-        'template': 'libraries/require/plugins/template',
-        'text': 'libraries/require/plugins/text',
-        'underscore': 'libraries/underscore/underscore',
-        'underscoreTemplate': 'libraries/underscore/extensions/template'
-    },
-    shim: {},
-    deps: [
-        'backboneStickit',
-        'jqueryFunctions',
-        'log',
-        'underscoreTemplate'
-    ],
-    callback: function() {
-        require([
-            'appular'
-        ], function(Appular) {
-            Appular.render();
-        });
+  waitSeconds: 0,
+  baseUrl: 'javascripts',
+  config: {
+    'appular': {
+      'env': 'dev'
     }
+  },
+  paths: {
+    'appular': 'libraries/appular/appular',
+    'backbone': 'libraries/backbone/backbone',
+    'backboneStickit': 'libraries/backbone/extensions/stickit',
+    'domReady': 'libraries/require/plugins/domReady',
+    'jquery': 'libraries/jquery/jquery',
+    'jqueryFunctions': 'libraries/jquery/extensions/functions',
+    'log': 'libraries/log/log',
+    'template': 'libraries/require/plugins/template',
+    'text': 'libraries/require/plugins/text',
+    'underscore': 'libraries/underscore/underscore',
+    'underscoreTemplate': 'libraries/underscore/extensions/template'
+  },
+  shim: {},
+  deps: [
+    'backboneStickit',
+    'jqueryFunctions',
+    'log',
+    'underscoreTemplate'
+  ],
+  callback: function() {
+    require([
+      'appular'
+    ], function(Appular) {
+      Appular.render();
+    });
+  }
 });
--- public/javascripts/libraries/require/configs/karma.js
+++ public/javascripts/libraries/require/configs/karma.js
@@ -2,45 +2,45 @@
  * Test Config Settings
  */
 var allTestFiles = [],
-    TESTS_REGEXP = /tests\.js$/,
-    pathToModule = function(path) {
-        return path.replace(/^\/base\//, '').replace(/\.js$/, '');
-    };
+  TESTS_REGEXP = /tests\.js$/,
+  pathToModule = function(path) {
+    return path.replace(/^\/base\//, '').replace(/\.js$/, '');
+  };
 
 Object.keys(window.__karma__.files).forEach(function(file) {
-    if (TESTS_REGEXP.test(file)) {
-        // Normalize paths to RequireJS module names.
-        allTestFiles.push(pathToModule(file));
-    }
+  if (TESTS_REGEXP.test(file)) {
+    // Normalize paths to RequireJS module names.
+    allTestFiles.push(pathToModule(file));
+  }
 });
 
 requirejs.config({
-    waitSeconds: 0,
-    baseUrl: '/base',
-    config: {
-        'appular': {
-            env: 'test',
-            useFixtures: true
-        }
-    },
-    paths: {
-        'appular': 'libraries/appular/appular',
-        'jquery': 'libraries/jquery/jquery',
-        'jqueryFunctions': 'libraries/jquery/extensions/functions',
-        'underscore': 'libraries/underscore/underscore',
-        'backbone': 'libraries/backbone/backbone',
-        'backboneStickit': 'libraries/backbone/extensions/stickit',
-        'bowser': 'libraries/bowser/bowser',
-        'domReady': 'libraries/require/plugins/domReady',
-        'async': 'libraries/require/plugins/async',
-        'json': 'libraries/require/plugins/json',
-        'template': 'libraries/require/plugins/template',
-        'text': 'libraries/require/plugins/text'
-    },
-    deps: [
-        'appular',
-        'jqueryFunctions',
-        'backboneStickit'
-    ].concat(allTestFiles),
-    callback: window.__karma__.start
+  waitSeconds: 0,
+  baseUrl: '/base',
+  config: {
+    'appular': {
+      env: 'test',
+      useFixtures: true
+    }
+  },
+  paths: {
+    'appular': 'libraries/appular/appular',
+    'jquery': 'libraries/jquery/jquery',
+    'jqueryFunctions': 'libraries/jquery/extensions/functions',
+    'underscore': 'libraries/underscore/underscore',
+    'backbone': 'libraries/backbone/backbone',
+    'backboneStickit': 'libraries/backbone/extensions/stickit',
+    'bowser': 'libraries/bowser/bowser',
+    'domReady': 'libraries/require/plugins/domReady',
+    'async': 'libraries/require/plugins/async',
+    'json': 'libraries/require/plugins/json',
+    'template': 'libraries/require/plugins/template',
+    'text': 'libraries/require/plugins/text'
+  },
+  deps: [
+    'appular',
+    'jqueryFunctions',
+    'backboneStickit'
+  ].concat(allTestFiles),
+  callback: window.__karma__.start
 });
